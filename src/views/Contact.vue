298faf832421c1333a98de8fc91a245bd273854f
Giovana <giovanadandrade@gmail.com>
2021-06-30 16:15:18 -0300

    create contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,17 +1,48 @@
 <template>
-  <div class="contato">
+  <section class="contact">
     <h2>{{ getterLanguage.contactPage.title }}</h2>
-    <div class="contatoicones">
+    <div class="contact-icons">
       <div
         v-for="(contact, index) in getterLanguage.contactPage.contacts"
         :key="index"
       >
-        <a class="icons" :href="contact.url" target="_blank">
+        <a :href="contact.url" target="_blank">
           <i :class="contact.class" />
         </a>
       </div>
     </div>
-  </div>
+    <form
+      class="email-form"
+      action="https://formspree.io/f/myylppve"
+      method="POST"
+    >
+      <label for="name">
+        <input
+          id="name"
+          type="text"
+          name="name"
+          :placeholder="getterLanguage.contactPage.form.name"
+        />
+      </label>
+      <label for="e-mail">
+        <input
+          id="email"
+          type="email"
+          name="_replyto"
+          :placeholder="getterLanguage.contactPage.form.email"
+        />
+      </label>
+      <label for="message">
+        <textarea
+          id="message"
+          name="message"
+          :placeholder="getterLanguage.contactPage.form.message"
+          rows="5"
+        ></textarea>
+      </label>
+      <button type="submit">Send</button>
+    </form>
+  </section>
 </template>
 
 <script>
@@ -23,21 +54,9 @@ export default {
 </script>
 
 <style>
-@media (max-width: 768px) {
-  .contato {
-    width: 80%;
-    height: 30%;
-  }
-}
-
-@media (min-width: 768px) {
-  .contato {
-    width: 50%;
-  }
-}
-
-.contato {
+.contact {
   height: 70vh;
+  width: 60%;
   margin-left: auto;
   margin-right: auto;
   display: flex;
@@ -45,17 +64,68 @@ export default {
   justify-content: center;
 }
 
-.contato h2 {
+.contact h2 {
   text-align: center;
   padding-bottom: 2rem;
 }
 
-.contatoicones {
+.contact-icons {
   display: flex;
   justify-content: space-around;
 }
 
-.icons i {
+.contact-icons i {
   font-size: 3rem;
+}
+.email-form {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 40vh;
+}
+
+.email-form input {
+  border: none;
+  border-bottom: solid #5a0d5c 0.15em;
+  text-decoration: none;
+  color: #5a0d5c;
+  width: 100%;
+  padding: 0.5em;
+  margin: 0.5em;
+}
+
+.email-form textarea {
+  resize: none;
+  width: 100%;
+  border: none;
+  border-bottom: solid #5a0d5c 0.15em;
+  margin: 0.5em;
+}
+
+.email-form button {
+  margin: 0.5em;
+  padding: 0.7em;
+  background: #ffffff;
+  border: solid #5a0d5c 0.15em;
+  -webkit-border-radius: 0.5em;
+  -moz-border-radius: 0.5em;
+  border-radius: 0.5em;
+  text-decoration: none;
+  color: #5a0d5c;
+  cursor: pointer;
+  text-align: center;
+  transition: all 1s;
+  width: fit-content;
+}
+
+.email-form button:hover,
+.email-form button:active {
+  -webkit-border-radius: 0.5em;
+  -moz-border-radius: 0.5em;
+  border-radius: 0.5em;
+  color: #ffffff;
+  background-color: #5a0d5c;
+  border: solid #5a0d5c 0.15em;
+  text-decoration: none;
 }
 </style>
