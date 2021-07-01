05603ba96905cb13fd4ee1a75a9a437956d763b6
giovanaandrade <giovanadandrade@gmail.com>
2021-07-01 16:46:43 -0300

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+git push -f git@github.com:giovanaandrade/giovanaandrade.github.io.git main:gh-pages
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
+cd -
